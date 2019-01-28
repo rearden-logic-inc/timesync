e679bc10b2f1429daf7c9b65924379948fc1d28b
Robert Robinson <rerobins@gmail.com>
2019-01-28 10:13:55 -0700

    Allow specifying alternate configuration files
    - Use the -c or --config options.

--- timesync/main.py
+++ timesync/main.py
@@ -3,12 +3,17 @@ Command line entry point for the script.
 """
 import argparse
 import logging
+import sys
 
 from timesync.subcommands import task_assignments, process
+from timesync.utils import configuration
 
 
 def _build_arguments():
     parser = argparse.ArgumentParser(description='Time Sync Command')
+
+    parser.add_argument('-c', '--config',
+                        help=f'Specify a configuration file, default is {configuration.DEFAULT_CONFIGURATION_FILE}')
 
     subparsers = parser.add_subparsers(help='sub-command help')
     task_assignments.create_argument_parser(subparsers)
@@ -22,10 +27,18 @@ def main():
     parser = _build_arguments()
 
     args = parser.parse_args()
-    if hasattr(args, 'func'):
-        args.func(args)
-    else:
-        parser.print_help()
+
+    try:
+        # Load up the configuration file if the argparser has a configuration file to load.
+        if args.config is not None:
+            configuration.get_configuration(args.config)
+
+        if hasattr(args, 'func'):
+            args.func(args)
+        else:
+            parser.print_help()
+    except RuntimeError:
+        sys.exit(-1)
 
 
 if __name__ == '__main__':
