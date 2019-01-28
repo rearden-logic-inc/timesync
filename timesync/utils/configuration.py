a8427c4fc4e32727fc8b3a7f7e6a6fbd7d844cbf
Robert Robinson <rerobins@gmail.com>
2019-01-28 10:01:07 -0700

    Add date time parsing
    allow parsing of 'today', 'yesterday', 'range', and 'YYYY-MM-DD' values.

--- timesync/utils/configuration.py
+++ timesync/utils/configuration.py
@@ -1,9 +1,12 @@
 """
 Module for loading and maintaining configuration values.
 """
+from datetime import datetime, timedelta
+
 from ruamel.yaml import YAML
 
 _configuration = None
+DATE_FORMAT_STRING = '%Y-%m-%d'
 
 
 def get_configuration(configuration_path='./config.yaml'):
@@ -21,3 +24,32 @@ def get_configuration(configuration_path='./config.yaml'):
             _configuration = parser.load(configuration_file)
 
     return _configuration
+
+
+def parse_date(configuration, date_key='date', start_key='start', end_key='end'):
+    """
+    Parse a date value out of the configuration dictionary provided using the provided keys.  Always returns a list of
+    dates.
+    :param dict configuration:
+    :param str date_key:
+    :param str start_key:
+    :param str end_key:
+    :return: list of dates
+    """
+    return_value = []
+
+    if configuration[date_key] == 'today':
+        return_value.append(datetime.now())
+    elif configuration[date_key] == 'yesterday':
+        return_value.append(datetime.now() - timedelta(1))
+    elif configuration[date_key] == 'range':
+        current_date = datetime.strptime(configuration[start_key], DATE_FORMAT_STRING)
+        end_date = datetime.strptime(configuration[end_key], DATE_FORMAT_STRING)
+
+        while current_date <= end_date:
+            return_value.append(current_date)
+            current_date += timedelta(1)
+    else:
+        return_value.append(datetime.strptime(configuration[date_key], DATE_FORMAT_STRING))
+
+    return return_value
