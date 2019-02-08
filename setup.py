46aaacbae38663656a746ec6a042c2bbe6028544
Robert Robinson <rerobins@gmail.com>
2019-02-08 09:40:20 -0700

    Start 0.4.0 development

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(LOCAL_PATH, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='timesync',
-    version='0.3.0',
+    version='0.4.0dev',
     packages=find_packages(),
     url='https://github.com/rerobins/timesync',
     license='Apache 2',
